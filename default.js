fc36bc56b942a4ee5b8a8a5a18f5f301daeba443
Endrik Tombak <littleendu@gmail.com>
2021-12-24 22:19:32 +0200

    Should be working version

--- default.js
+++ default.js
@@ -1,1 +1,46 @@
 // TODO: implement furigana hider
+
+function repeat(func, times) {
+    func();
+    times && --times && repeat(func, times);
+}
+
+function removeEmpty(targetNode = null) {
+    targetNode.childNodes.forEach((el) => {
+        removeEmpty(el)
+        if (el.textContent === "" && el.childNodes.length === 0 && el.nodeName !== "BR") {
+            el.remove()
+        }
+    })
+}
+
+function mergeSimilar(targetNode) {
+    if (!targetNode) return
+    if (targetNode.nodeName === "#text") return;
+
+
+    while (targetNode.nextSibling
+    && targetNode.nodeName === targetNode.nextSibling.nodeName
+    && targetNode.className === targetNode.nextSibling.className) {
+        targetNode.append(...targetNode.nextSibling.childNodes)
+        targetNode.nextSibling.remove()
+    }
+
+
+    for (let c of targetNode.children) {
+        mergeSimilar(c)
+    }
+}
+
+function formatParagraph() {
+    repeat(() => {
+        let p = document.getElementById('lyrics')
+        p.innerHTML = p.innerHTML
+            .replaceAll(/\n\s*/g, '')
+            .replaceAll('<br>', '<br>\n')
+        removeEmpty(document.getElementById('lyrics'))
+        mergeSimilar(document.getElementById('lyrics'))
+    }, 2)
+}
+
+document.addEventListener("DOMContentLoaded", formatParagraph)
--- tools/editor.js
+++ tools/editor.js
@@ -25,78 +25,111 @@ function walkSiblings(callback = (cur) => console.log(cur)) {
     }
 }
 
-// TODO: bug in firefox, https://bugzilla.mozilla.org/show_bug.cgi?id=1746926
+function createSelectionSpans(range) {
+    // TODO: bug in firefox, https://bugzilla.mozilla.org/show_bug.cgi?id=1746926
+    // walk out of <rt>
+    let startNode = range.startContainer
+    while (startNode.parentNode.nodeName !== "P") {
+        startNode = startNode.parentNode
+        if (startNode.nodeName === "RUBY") break
+    }
+
+    let endNode = range.endContainer
+    while (endNode.parentNode.nodeName !== "P") {
+        endNode = endNode.parentNode
+        if (endNode.nodeName === "RUBY") break
+    }
+
+    let startSpan = document.createElement('span')
+    startSpan.id = 'start'
+    let endSpan = document.createElement('span')
+    endSpan.id = 'end'
+
+    if (startNode === endNode && range.commonAncestorContainer.nodeName !== "RUBY") {
+        let node = range.commonAncestorContainer
+        let start = node.textContent.substring(0, range.startOffset)
+        let selected = node.textContent.substring(range.startOffset, range.endOffset)
+        let end = node.textContent.substring(range.endOffset)
+        node.replaceWith(start, startSpan, selected, endSpan, end)
+    } else {
+        // start
+        if (startNode.nodeName === "RUBY") {
+            startNode.before(startSpan)
+        } else if (range.startContainer.nodeName === "#text") {
+            let node = range.startContainer
+            let start = node.textContent.substring(0, range.startOffset)
+            let selected = node.textContent.substring(range.startOffset)
+            node.replaceWith(start, startSpan, selected)
+        } else {
+            console.error("can't create start span")
+        }
+
+        // end
+        if (endNode.nodeName === "RUBY") {
+            endNode.after(endSpan)
+        } else if (range.endContainer.nodeName === "#text") {
+            let node = range.endContainer
+            let selected = node.textContent.substring(0, range.endOffset)
+            let end = node.textContent.substring(range.endOffset)
+            node.replaceWith(selected, endSpan, end)
+        } else {
+            console.error("can't create end span")
+            startSpan.remove()
+        }
+    }
+}
+
+function makeChildOfP(targetSpan) {
+    while (targetSpan.parentElement.nodeName !== "P") {
+        let children = Array.from(targetSpan.parentElement.childNodes)
+        let index = children.indexOf(targetSpan)
+        let start = children.slice(0, index)
+        let end = children.slice(index + 1)
+
+        let name = targetSpan.parentElement.nodeName
+        let startElement = document.createElement(name)
+        startElement.className = targetSpan.parentElement.className
+        startElement.append(...start)
+        let endElement = document.createElement(name)
+        endElement.className = targetSpan.parentElement.className
+        endElement.append(...end)
+
+        targetSpan.parentElement.replaceWith(startElement, targetSpan, endElement)
+    }
+}
+
+function unwrapMarks(targetNode) {
+    targetNode.childNodes.forEach((el) => {
+        if (el.nodeName === "MARK")
+            el.replaceWith(...el.childNodes)
+        unwrapMarks(el)
+    })
+}
+
+function markSelectionSpans(targetColor) {
+    let startSpan = document.getElementById('start')
+    let endSpan = document.getElementById('end')
+
+    makeChildOfP(startSpan)
+    makeChildOfP(endSpan)
+    let children = Array.from(startSpan.parentElement.childNodes)
+    let startIndex = children.indexOf(startSpan)
+    let endIndex = children.indexOf(endSpan)
+    let selection = children.slice(startIndex + 1, endIndex)
+    let selectedElement = document.createElement('mark')
+    selectedElement.className = targetColor
+    selectedElement.append(...selection)
+    unwrapMarks(selectedElement)
+    startSpan.replaceWith(selectedElement)
+    endSpan.remove()
+}
+
 function applyMark(targetColor = 'red') {
     let selection = window.getSelection()
     for (let i = 0; i < selection.rangeCount; i++) {
         let range = selection.getRangeAt(i)
-
-        let startNode = range.startContainer
-        while (startNode.parentNode.nodeName !== "P") {
-            startNode = startNode.parentNode
-            if (startNode.nodeName === "RUBY") break
-        }
-
-        let endNode = range.endContainer
-        while (endNode.parentNode.nodeName !== "P") {
-            endNode = endNode.parentNode
-            if (endNode.nodeName === "RUBY") break
-        }
-
-        let startSpan = document.createElement('span')
-        startSpan.id = 'start'
-        let endSpan = document.createElement('span')
-        endSpan.id = 'end'
-
-        if (startNode === endNode && range.commonAncestorContainer.nodeName !== "RUBY") {
-            let node = range.commonAncestorContainer
-            let start = node.textContent.substring(0, range.startOffset)
-            let selected = node.textContent.substring(range.startOffset, range.endOffset)
-            let end = node.textContent.substring(range.endOffset)
-            node.replaceWith(start, startSpan, selected, endSpan, end)
-        } else {
-            // start
-            if (startNode.nodeName === "RUBY") {
-                startNode.before(startSpan)
-            } else {
-                let node = range.startContainer
-                let start = node.textContent.substring(0, range.startOffset)
-                let selected = node.textContent.substring(range.startOffset)
-                node.replaceWith(start, startSpan, selected)
-            }
-
-            // end
-            if (endNode.nodeName === "RUBY") {
-                endNode.after(endSpan)
-            } else {
-                let node = range.endContainer
-                let selected = node.textContent.substring(0, range.endOffset)
-                let end = node.textContent.substring(range.endOffset)
-                node.replaceWith(selected, endSpan, end)
-            }
-        }
+        createSelectionSpans(range)
+        markSelectionSpans(targetColor)
+        formatParagraph()
     }
 }
-
-// you can check parentNode.nodeName === "MARK" (this is no longer true, use currentNode.nodeName === "MARK")
-// you can use nextSibling to walk from start to end, to check if any of them are "MARK"
-// if neither just replace startContainer with ${start}<mark class="red">${a} and endContainer with ${b}</mark>${end}
-
-// you can just remove (unwrap with jquery?) any MARKs you encounter while walking from start to end
-// that is if neither start nor end are themselves inside a MARK
-
-// if partially inside a MARK, move the end tag to start or vice-versa
-
-// if inside another tag, you need to make the part that's selected in the tag MARK and then one of the tag limits MARK
-// </ruby><mark class="red> for end, </mark><ruby> for start
-// need to do this for both the start and end of selection
-
-function formatParagraph() {
-    let paragraphs = document.getElementsByTagName('p')
-    for (let p of paragraphs) {
-        p.innerHTML = p.innerHTML
-            .replaceAll(/\n\s*/g, '')
-            .replaceAll('<br>', '<br>\n')
-    }
-}
-
